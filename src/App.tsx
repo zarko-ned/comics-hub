2632599c8ddb4770d50e37c807b8069f78ff712c
Zarko <zarkoned@outloo.com>
2025-02-14 14:57:11 +0100

    added dark theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,32 @@
-import { Grid, GridItem, Show } from "@chakra-ui/react";
+import React from "react";
+import { ChakraProvider, defaultSystem, Grid, GridItem } from "@chakra-ui/react";
+import { ColorModeProvider } from "@/components/ui/color-mode";
 import { NavBar } from "./components/ui/NavBar";
+import { ColorModeButton } from "@/components/ui/color-mode";
 
 function App() {
   return (
-    <Grid
-      templateAreas={{
-        base: `"nav" "main"`,
-        lg: `"nav nav" "aside main"`,
-      }}
-    >
-      <GridItem area="nav">
-        <NavBar/>
-      </GridItem>
-      <GridItem area="aside" bg="gold" display={{ base: "none", lg: "block" }}>
-        Aside
-      </GridItem>
-      <GridItem area="main" bg="dodgerblue">
-        Main
-      </GridItem>
-    </Grid>
+    <ChakraProvider value={defaultSystem}>
+      <ColorModeProvider>
+        <Grid
+          templateAreas={{
+            base: `"nav" "main"`,
+            lg: `"nav nav" "aside main"`,
+          }}
+        >
+          <GridItem area="nav" display="flex" justifyContent="space-between" alignItems="center">
+            <NavBar />
+            <ColorModeButton />
+          </GridItem>
+          <GridItem area="aside" bg="gold" display={{ base: "none", lg: "block" }}>
+            Aside
+          </GridItem>
+          <GridItem area="main" bg="dodgerblue">
+            Main
+          </GridItem>
+        </Grid>
+      </ColorModeProvider>
+    </ChakraProvider>
   );
 }
 
