5b99083a9996a1369d20d8029cfa0a6883636820
Zarko <zarkoned@outloo.com>
2025-02-15 11:37:46 +0100

    dark theme default

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,46 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { ChakraProvider, defaultSystem, Grid, GridItem } from "@chakra-ui/react";
-import { ColorModeProvider } from "@/components/ui/color-mode";
+import { ColorModeProvider, useColorMode } from "@/components/ui/color-mode";
 import { NavBar } from "./components/ui/NavBar";
 import { ColorModeButton } from "@/components/ui/color-mode";
 import ComicGrid from "./components/ComicGrid";
 import SeriaList from "./components/SeriaList";
+import { ThemeProvider } from "next-themes";
 
 function App() {
+  const { colorMode, setColorMode } = useColorMode();
+
+  useEffect(() => {
+    // Postavi default temu na dark samo jednom, pri inicijalnom renderovanju
+    if (colorMode !== "dark") {
+      setColorMode("dark");
+    }
+  }, []); // Prazan niz zavisnosti znači da se useEffect pokreće samo jednom, pri mountu
+
   return (
-    <ChakraProvider value={defaultSystem}>
-      <ColorModeProvider>
-        <Grid
-          templateAreas={{
-            base: `"nav" "main"`,
-            lg: `"nav nav" "aside main"`,
-          }}
-        >
-          <GridItem area="nav" display="flex" justifyContent="space-between" alignItems="center">
-            <NavBar />
-            <ColorModeButton />
-          </GridItem>
-          <GridItem area="aside"  display={{ base: "none", lg: "block" }}>
-            <SeriaList/>
-          </GridItem>
-          <GridItem area="main" >
-           <ComicGrid/>
-          </GridItem>
-        </Grid>
-      </ColorModeProvider>
-    </ChakraProvider>
+    <ThemeProvider attribute="class" defaultTheme="dark">
+      <ChakraProvider value={defaultSystem}>
+        <ColorModeProvider>
+          <Grid
+            templateAreas={{
+              base: `"nav" "main"`,
+              lg: `"nav nav" "aside main"`,
+            }}
+          >
+            <GridItem area="nav" display="flex" justifyContent="space-between" alignItems="center">
+              <NavBar />
+              <ColorModeButton />
+            </GridItem>
+            <GridItem area="aside" display={{ base: "none", lg: "block" }}>
+              <SeriaList />
+            </GridItem>
+            <GridItem area="main">
+              <ComicGrid />
+            </GridItem>
+          </Grid>
+        </ColorModeProvider>
+      </ChakraProvider>
+    </ThemeProvider>
   );
 }
 
